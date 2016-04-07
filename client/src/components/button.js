f929405104512b09efdf75360a6cee1bff3e4280
seanagibson <seanandgibson@gmail.com>
2016-04-07 11:41:37 -0700

    refac: fixes issues

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-var Button = function({data, callback}){
-  return (
-    <div>
-      <button onClick={callback}>{data.text}</button>
-    </div>
-  )
+export default class Button extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.callback();
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.handleSubmit.bind(this)}>{this.props.data.text}</button>
+      </div>
+    )
+  }
 };
 
 export default Button;
