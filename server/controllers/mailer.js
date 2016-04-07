d671f5bc4f5021a0e008ade3e24ad1cc10833f2b
Elliot Chi <elliotschi@gmail.com>
2016-04-06 18:11:17 -0700

    feat: add validation to mailer controller function

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -3,6 +3,8 @@ const mailer = require('./docuSignMailer.js');
 const async = require('async');
 module.exports = {
     massMailer: function(req, res) {
+      // validation
+      if (req.body.emailList && req.body.docuSignEmail && req.body.docuSignPass && req.body.volunteerInfo && req.body.templateInfo) {
         const emailList = req.body.emailList;
         const docuSignEmail = req.body.docuSignEmail;
         const docuSignPass = req.body.docuSignPass;
@@ -12,5 +14,6 @@ module.exports = {
             mailer(docuSignEmail, docuSignPass, volunteerInfo, templateInfo);
         });
         res.send('Operation Complete');
+      }
     }
 };
