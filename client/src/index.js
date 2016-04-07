8b9647fcd84a6f191a30202ad9ab48c1c47941e6
seanagibson <seanandgibson@gmail.com>
2016-04-06 17:05:52 -0700

    feat: adds home component

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import redux from 'redux';
 import {Provider} from 'react-redux';
-import {Router, Route} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import configStore from './store/configStore';
 import App from './component/App';
+import Home from './component/Home';
 
 const store = configStore();
 const history = syncHistoryWithStore(hashHistory, store);
@@ -14,7 +15,9 @@ const history = syncHistoryWithStore(hashHistory, store);
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={App}></Route>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home}/>
+      </Route>
     </Router>
   </Provider>
 , document.getElementById('app'));
